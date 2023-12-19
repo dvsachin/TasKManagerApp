1dab666953bb0aee5e0b8b594e5f08fac07622f0
dvsachin <dvsachin9@gmail.com>
2023-12-19 19:25:35 +0530

    App initialized

---
+++ src/test/java/com/manager/taskmanager_spring/TaskManagerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manager.taskmanager_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
